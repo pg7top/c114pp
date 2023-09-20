3c38651c516de7971b05f29260b167869f1c017d
pg7top <111295676+pg7top@users.noreply.github.com>
2023-09-20 20:35:19 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,15 @@
+
+
+
+a = 33
+b = "33"
+
+
+b = int(b)
+
+if b > a:
+    print("b é maior que a")
+elif b == a:
+    print("a e b são iguais")
+else:
+    print("a é maior que b")
---
+++ teste2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emilly")
+my_function("Tobias")
+my_function("Lucas")
